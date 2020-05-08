be307ef540ff26532c7ec67815b486578a166bbb
luxingy <xingyu-lu@outlook.com>
2020-05-08 12:26:01 -0700

    add generic data retrieval

--- pinpointwechat/app.js
+++ pinpointwechat/app.js
@@ -9,19 +9,16 @@ var api = new WechatAPI(appid, appsecret);
 
 exports.lambda_handler = (event, context, callback) => {
     //templateId determines the template you will be using to configure the message
-    var templateId = event.templateId
     //customer will be redirected to this url after they click the message
-    var url = event.url;
     //openId is customer's wechat account userid
-    var openid = event.openid;
+    var { templateId, url, openid, ...data } = event;
+
     //request body which contains data to be put into the template
-    var data = {
-        "serviceName": {
-          "value": event.serviceName,
-          "color": "#173177"
-        }
-    };
-    api.sendTemplate(openid, templateId, url, data, function (err, res) {
+    var body = {};
+    Object.keys(data).forEach(item => {
+        body[item] = { "value": data[item] };
+    });
+    api.sendTemplate(openid, templateId, url, body, function (err, res) {
     if (err) return console.error(err);
     console.log(JSON.stringify(res));
 });
