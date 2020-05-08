c018382819704e41fd50744074c09403b8c54618
luxingy <xingyu-lu@outlook.com>
2020-05-07 20:52:33 -0700

    remvoe template, add wechat channel lambdaFunc

--- hello-world/app.js
+++ hello-world/app.js
@@ -1,33 +1,26 @@
-// const axios = require('axios')
-// const url = 'http://checkip.amazonaws.com/';
-let response;
+'use strict';
 
-/**
- *
- * Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
- * @param {Object} event - API Gateway Lambda Proxy Input Format
- *
- * Context doc: https://docs.aws.amazon.com/lambda/latest/dg/nodejs-prog-model-context.html 
- * @param {Object} context
- *
- * Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
- * @returns {Object} object - API Gateway Lambda Proxy Output Format
- * 
- */
-exports.lambdaHandler = async (event, context) => {
-    try {
-        // const ret = await axios(url);
-        response = {
-            'statusCode': 200,
-            'body': JSON.stringify({
-                message: 'hello world',
-                // location: ret.data.trim()
-            })
-        }
-    } catch (err) {
-        console.log(err);
-        return err;
-    }
+console.log('Loading function');
+var WechatAPI = require('wechat-api');
 
-    return response
+var appid = process.env.APP_ID
+var appsecret = process.env.APP_SECRET
+var api = new WechatAPI(appid, appsecret);
+
+exports.lambda_handler = (event, context, callback) => {
+    // var templateId = '7BCdeF9hIJ0Kl-aB6KvJ59ZM7jfj6fBIf1OL_-dDFb6'
+    var templateId = event.templateId
+    console.log(">>>>>>>>");
+    console.log(templateId);
+    // URL置空，则在发送后,点击模板消息会进入一个空白页面（ios）, 或无法点击（android）
+    // var url =  'z.cn';
+    var url = event.url;
+    // var openid = 'opDi3TmaFiF98ABCDEF_9M9nHJK';
+    var openid = event.openid;
+    var data = {};
+    api.sendTemplate(openid, templateId, url, data, function (err, res) {
+    if (err) return console.error(err);
+    console.log(JSON.stringify(res));
+});
+    callback(null, event);
 };
